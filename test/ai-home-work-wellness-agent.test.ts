c7f1ced14979ec54dd799a811e88a2a83934ca86
Saxon-Pi <ts.8849.p@gmail.com>
2026-03-23 22:49:15 +0900

    first commit

---
+++ test/ai-home-work-wellness-agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiHomeWorkWellnessAgent from '../lib/ai-home-work-wellness-agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai-home-work-wellness-agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiHomeWorkWellnessAgent.AiHomeWorkWellnessAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
